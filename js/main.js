a07f0aaf8b90d6acfb03f0c0238ea8b3c1fb80dd
JayoonKoo <rnwkdbs12@gmail.com>
2021-05-25 00:00:21 +0900

    화면 top으로 가는 버튼 생성

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 const badgeEl = document.querySelector('header .badges');
-
+const toTopEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(function () {
   if (window.scrollY > 500) {
     // 배지 숨기기
@@ -24,16 +24,31 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display: 'none'
     });
+    // 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
   } else {
     // 배지 보여주기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
+    }); 
+    // 버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
     });
   }
 }, 300));
 //_.throttle(함수, 시간)
 
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    // gsamp scrollto plugin
+    scrollTo: 0
+  });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
